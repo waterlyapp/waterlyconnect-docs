ca35efc64b372c81b6ffac6d05bb856f579d3cd8
Michael Murdy <168657779+michael-murdy@users.noreply.github.com>
2026-07-14 08:36:01 -0500

    Updating url reference and setting proper logger handling

--- data-submission-api/clients/packages/ignition/ignition_to_waterly/ignition/script-python/waterly/code.py
+++ data-submission-api/clients/packages/ignition/ignition_to_waterly/ignition/script-python/waterly/code.py
@@ -11,7 +11,7 @@ waterly_device_id = '<WATERLY_DEVICE_ID>'
 waterly_device_token = "<WATERLY_DEVICE_TOKEN>"
 
 system_tags = [
-	"[System]Gateway/CurrentDateTime", 
+	"[System]Gateway/CurrentDateTime",
 	"[System]Gateway/Timezone",
 	"[System]Gateway/UptimeSeconds"
 ]
@@ -23,15 +23,15 @@ def sendDataToWaterly(tags=None):
 	tags = tags or []
 	if isinstance(tags, str):
 		tags = [tags]
-	
+
 	tags.extend(system_tags)
 	# read values
 	tag_values = system.tag.readBlocking(tags)
 	# set timestamp
 	now = int(time.time())
-	
+
 	submission_tags = []
-	
+
 	for idx, tag in enumerate(tag_values):
 		tag_name = tags[idx]
 		if tag.quality == QualityCode.Good:
@@ -42,37 +42,34 @@ def sendDataToWaterly(tags=None):
 			})
 	body={
 		"device" : {
-	 		"id" : waterly_device_id,
-	 		"type" : "Ignition"
+			"id" : waterly_device_id,
+			"type" : "Ignition"
 		},
-	"timestamp" : now,
-	"tags" : submission_tags
+		"timestamp" : now,
+		"tags" : submission_tags
 	}
-	
-	
 
-	url = "https://app.waterlyapp.com/connect/submit"
 	json_payload = system.util.jsonEncode(body)
 	headers = {
-	    "x-waterly-request-type": "WaterlyConnect",
-	    "x-waterly-connect-token": waterly_device_token
+		"x-waterly-request-type": "WaterlyConnect",
+		"x-waterly-connect-token": waterly_device_token
 	}
 	try:
 		# use 'alternate' syntax for httpPost, explicitly defining parameters
 		response = system.net.httpPost(
-		    url,
-		    "application/json",      # contentType
-		    json_payload,            # postData 
-		    10000,                   # connectTimeout
-		    60000,                   # readTimeout
-		    None, None,              # 
-		    headers,                 # headerValues 
-		    False,					 # bypass cert validation
-		   	True					 # throw on error
+			waterly_api_url,
+			"application/json",      # contentType
+			json_payload,            # postData
+			10000,                   # connectTimeout
+			60000,                   # readTimeout
+			None, None,              #
+			headers,                 # headerValues
+			False,					 # bypass cert validation
+			True					 # throw on error
 		)
-		logging.info("Successful Post to WaterlyConnect" + response)  #for debugging
+		logger.info("Successful Post to WaterlyConnect" + response)  #for debugging
 	except Exception as e:
-		logging.error("Error posting to WaterlyConnect: %s" % str(e))
+		logger.error("Error posting to WaterlyConnect: %s" % str(e))
 	
 
 
